59625b9c6905108a5488bf0fd229b3c1acbe415d
J-Manuel-GB <[juanmanuelgarciabola]>
2025-09-24 18:06:24 -0600

    ORIGINAL

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -13,26 +13,32 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                        <flux:navlist.item icon="folder" 
-        :href="route('plantillas.index')" 
-        :current="request()->routeIs('plantillas.*')" 
-        wire:navigate>
-        {{ __('Plantillas') }}
+                    <flux:navlist.item icon="home" 
+                        :href="route('dashboard')" 
+                        :current="request()->routeIs('dashboard')" 
+                        wire:navigate>
+                        {{ __('Dashboard') }}
+                    </flux:navlist.item>
+
+                    <!-- Nuevo apartado de Plantillas -->
+                    <flux:navlist.item icon="folder" 
+                        :href="route('plantillas.index')" 
+                        :current="request()->routeIs('plantillas.*')" 
+                        wire:navigate>
+                        {{ __('Plantillas') }}
+                    </flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
-
-            
 
             <flux:spacer />
 
             <flux:navlist variant="outline">
                 <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
+                    {{ __('Repository') }}
                 </flux:navlist.item>
 
                 <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
+                    {{ __('Documentation') }}
                 </flux:navlist.item>
             </flux:navlist>
 
